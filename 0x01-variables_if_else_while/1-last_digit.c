4f64e4b212a556468d478b6b589bb44653fc7d26
Nassima Lkhaili <lkhailinassima@gmail.com>
2023-03-17 14:04:49 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ lastd = n % 10;
 
 if (lastd > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, last
+printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 
 }
 else if (lastd == 0)
